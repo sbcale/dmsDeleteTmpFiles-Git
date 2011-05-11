13121fd9a7f0db470a83f33ef0b8bc6fa4e0661a
scale <sbcale@gmail.com>
2011-05-11 12:31:17 -0400

    Added my comment for me.

--- modMain.vb
+++ modMain.vb
@@ -50,7 +50,7 @@ Module modMain
 
         ' get command line
         strCommandLine = My.Application.CommandLineArgs(0).ToString.Trim
-        ' hardcoded temp
+        ' hardcoded temp SBC Merge test
         'strCommandLine = "C:\Inetpub\wwwroot\publish\PDF\*.pdf;30"
 
         ' check command line
