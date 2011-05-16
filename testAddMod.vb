1e7c7ae6529e77d0b00f79388607690a5535a670
sbcale <sbcale@gmail.com>
2011-05-16 19:04:38 -0400

    Added new function RmNull in testAddMod.vb.

--- testAddMod.vb
+++ testAddMod.vb
@@ -33,4 +33,32 @@
 
         dmsV7Util_checkForInvalidCharsForFilenames = True
     End Function
+
+    Function RmNull(ByVal CharStr As String) As String
+
+        ' This function will truncate 'CharStr' at the first NULL character found.
+
+        ' Local variable declarations.
+
+        Dim NullPos As Integer
+        Dim tmpCharStr As String
+
+        ' Find the first NULL character.
+
+        tmpCharStr = CharStr
+
+        NullPos = InStr(1, tmpCharStr, vbNullChar)
+
+        ' Check 'NullPosition'.
+
+        If NullPos < 1 Then tmpCharStr = Trim$(tmpCharStr)
+        If NullPos = 1 Then tmpCharStr = ""
+        If NullPos > 1 Then tmpCharStr = Trim$(Left$(tmpCharStr, NullPos - 1))
+
+        ' The function value is never checked by the calling routine.  This should be a subroutine.
+
+        RmNull = tmpCharStr
+
+    End Function
+
 End Module
